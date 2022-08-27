37cacf3144588295a166db42acfaf1b68eff9774
ShyamSundhar1411 <clashwithchiefrpjyt@gmail.com>
2022-08-27 14:58:06 +0530

    Library and Types

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,4 @@
+use hello::greet;
 fn main() {
-    let number: i32 = 2;
-    const FACTOR:f64 = 9.9;
-    println!("{},{}",number,FACTOR);
-    let x :i32 = 5;
-    {
-        let x:i32 = 40;
-        println!("{}",x);
-    }
-    println!("{}",x);
+    greet();
 }
