d40670865be606c070b7ebf1dab9da8e26b5f891
ShyamSundhar1411 <clashwithchiefrpjyt@gmail.com>
2022-08-28 23:40:55 +0530

    Strings and Con trol Flows

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,13 @@
 use hello::greet;
 fn main() {
     greet();
+    let num:i32 = 5;
+    let mut msg:&str;
+    msg = if num==5{
+        "five"
+    }
+    else {
+        "four"
+    };
+    println!("{}",msg);
 }
